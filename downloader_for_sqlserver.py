fa6b7cf275f7b86d7fd5ab6a5335d8f97d42badb
LeeXin <lixin2011@outlook.com>
2020-12-10 17:16:02 +0800

    对服务器中数据不存在报错的问题进行了修复

--- downloader_for_sqlserver.py
+++ downloader_for_sqlserver.py
@@ -14,8 +14,16 @@ def get_data_from_server(location,month,ip,user,password):
             query = 'SELECT * FROM [data].[dbo].[{:s}#站历史曲线M{:s}]'.format(str(location),str(month))
             cursor.execute(query)
             results = cursor.fetchall()
-            key_list = list(results[0].keys())
-            return results,key_list
+            try:
+                key_list = list(results[0].keys())
+                return results,key_list
+            except IndexError as e:
+                print('\n未查找到数据')
+                results = ''
+                key_list = None
+                return results,key_list
+
+            
 
 def get_points_definition_from_server(ip,user,password):
     '''
@@ -32,25 +40,30 @@ def get_points_definition_from_server(ip,user,password):
             return name_dict
 
 def export_data_to_csv(data,keys,name_dict,location_str,location,month):    
-    for key_index in range(len(keys)):
-        try:
-            if (keys[key_index] == 'AI0')|(keys[key_index] == 'AI1')|(keys[key_index] == 'AI2')|(keys[key_index] == 'AI3')|(keys[key_index] == 'AI4')|(keys[key_index] == 'AI5')| \
-                (keys[key_index] == 'AI6')|(keys[key_index] == 'AI7')|(keys[key_index] == 'AI8')|(keys[key_index] == 'AI9'):
-                keys[key_index] = name_dict[location_str + keys[key_index][:2] + '0' + keys[key_index][-1]]
-            elif (keys[key_index] == '年')|(keys[key_index] == '月')|(keys[key_index] == '日')|(keys[key_index] == 'SerialNumber')|(keys[key_index] == 'IDX'):
-                pass
-            else:
-                keys[key_index] = name_dict[location_str+keys[key_index]]
-        except KeyError as e:
-            print('未查找到该点定义：',e)
+    if data:
+        for key_index in range(len(keys)):
+            try:
+                if (keys[key_index] == 'AI0')|(keys[key_index] == 'AI1')|(keys[key_index] == 'AI2')|(keys[key_index] == 'AI3')|(keys[key_index] == 'AI4')|(keys[key_index] == 'AI5')| \
+                    (keys[key_index] == 'AI6')|(keys[key_index] == 'AI7')|(keys[key_index] == 'AI8')|(keys[key_index] == 'AI9'):
+                    keys[key_index] = name_dict[location_str + keys[key_index][:2] + '0' + keys[key_index][-1]]
+                elif (keys[key_index] == '年')|(keys[key_index] == '月')|(keys[key_index] == '日')|(keys[key_index] == 'SerialNumber')|(keys[key_index] == 'IDX'):
+                    pass
+                else:
+                    keys[key_index] = name_dict[location_str+keys[key_index]]
+            except KeyError as e:
+                print('未查找到该点定义：',e)
 
-    data_to_csv = pd.DataFrame(columns=keys)
-    for row in tqdm.tqdm(data):
-        row = pd.Series(list(row.values()),index=keys)
-        data_to_csv = data_to_csv.append(row,ignore_index=True)
-    file_name = str(location) + '#站历史曲线M' + str(month)
-    print('\n导出文件：' + file_name + '\n')
-    data_to_csv.to_csv(file_name +'.csv',encoding='GBK',index=False)
+        data_to_csv = pd.DataFrame(columns=keys)
+        for row in tqdm.tqdm(data):
+            row = pd.Series(list(row.values()),index=keys)
+            data_to_csv = data_to_csv.append(row,ignore_index=True)
+        file_name = str(location) + '#站历史曲线M' + str(month)
+        print('\n导出文件：' + file_name + '\n')
+        data_to_csv.to_csv(file_name +'.csv',encoding='GBK',index=False)
+    else:
+        file_name = str(location) + '#站历史曲线M' + str(month)
+        print('\n导出文件：' + file_name + '\n')
+        print('无法导出文件，因为sql无数据传入')
 
 if __name__ == "__main__":
     ip = input('输入SQL server的ip地址： ')
@@ -58,8 +71,8 @@ if __name__ == "__main__":
     password = input('输入密码： ')
     print('\n----------  开始传输数据  ----------\n')
     name_dict = get_points_definition_from_server(ip,user,password)
-    for location in range(18,35):
-        for month in range(1,13):
+    for location in range(1,35):
+        for month in range(9,13):
             location_str = '{:0>2s}'.format(str(location))
             all_data,keys = get_data_from_server(location,month,ip,user,password)
             export_data_to_csv(all_data,keys,name_dict,location_str,location,month)
